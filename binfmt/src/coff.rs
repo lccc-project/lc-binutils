72ab8a90b12cc9c1fff550b1aab78c63a4705c54
Connor <chorman64@gmail.com>
2021-04-21 12:09:26 -0400

    Start implementing some more formats

--- binfmt/src/coff.rs
+++ binfmt/src/coff.rs
@@ -1,1 +1,41 @@
+use bytemuck::{Pod, Zeroable};
 
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Zeroable, Pod)]
+pub struct CoffHeader {
+    mach: u16,
+    nsect: u16,
+    timestamp: u32,
+    symtab_off: u32,
+    n_symtab: u32,
+    opthead_size: u16,
+    characteristics: u16,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Zeroable, Pod)]
+pub struct CoffSectionHeader {
+    name: [u8; 8],
+    vsize: u32,
+    vaddr: u32,
+    fsize: u32,
+    foff: u32,
+    relocoff: u32,
+    lnoff: u32,
+    nreloc: u16,
+    nlines: u16,
+    characteristics: u32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Zeroable, Pod)]
+pub struct CoffOptionalHeader {
+    magic: u16,
+    link_maj: u8,
+    lin_min: u8,
+    code_size: u32,
+    init_size: u32,
+    uninit_size: u32,
+    entry_addr: u32,
+    code_base: u32,
+}
--- binfmt/src/macho.rs
+++ binfmt/src/macho.rs
@@ -1,1 +1,73 @@
+use bytemuck::{Pod, Zeroable};
 
+macro_rules! fake_enum{
+    {#[repr($t:ty)] $vis:vis enum $name:ident {
+        $($item:ident = $expr:literal),*$(,)?
+    }} => {
+        #[derive(Copy,Clone,Eq,PartialEq,Zeroable,Pod)]
+        #[repr(transparent)]
+        $vis struct $name($t);
+
+        $($vis const $item: $name = $name($expr);)*
+
+        impl ::core::fmt::Debug for $name{
+            #[allow(unreachable_patterns)]
+            fn fmt(&self,f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result{
+                match self{
+                    $(Self($expr) => f.write_str(::core::stringify!($item)),)*
+                    e => e.0.fmt(f)
+                }
+            }
+        }
+    }
+}
+
+fake_enum! {
+    #[repr(u32)]
+    pub enum CpuType{
+        Any = -1,
+        Vax = 1,
+        MC68k = 6,
+        X86 = 7,
+        X86_64 = 0xff000007,
+        MC98k = 10,
+        Hppa = 11,
+        Arm = 12,
+        Arm64 = 0xff00000c,
+        MC88k = 13,
+        Sparc = 14,
+        I860 = 15,
+        PowerPC = 16,
+        PowerPC64 = 0xff000010
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Zeroable, Pod)]
+pub struct MachOHeader32 {
+    magic: u32,
+    cputype: CpuType,
+    cpusubtype: u32,
+    filetype: u32,
+    ncmds: u32,
+    sizecmds: u32,
+    flags: u32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Zeroable, Pod)]
+pub struct MachOHeader64 {
+    magic: u32,
+    cputype: CpuType,
+    cpusubtype: u32,
+    filetype: u32,
+    ncmds: u32,
+    sizecmds: u32,
+    flags: u32,
+    reserved: u32,
+}
+
+pub const MH_MAGIC32: u32 = 0xfeedface;
+pub const MH_MAGIC64: u32 = 0xfeedfacf;
+pub const MH_REVERSED_MAGIC32: u32 = MH_MAGIC32.swap_bytes();
+pub const MH_REVERSED_MAGIC64: u32 = MH_MAGIC64.swap_bytes();
