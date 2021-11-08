b615cd765d58f08e52513988e5448e3fd4ccd0e7
Connor Horman <chorman64@gmail.com>
2021-11-08 10:46:44 -0500

    Add InsnRead and InsnWrite traits

--- arch-ops/src/traits.rs
+++ arch-ops/src/traits.rs
@@ -1,31 +1,27 @@
-use std::{
-    fmt::Debug,
-    io::{Read, Write},
-};
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
-pub enum Address {
-    Symbol(String),
+#[derive(Clone, Debug )]
+pub enum Address{
     Abs(u128),
-    Offset(String, i64),
+    Disp(i64),
+    Symbol{name: String, disp: i64}
 }
 
-pub trait InsnWrite: Write {
-    fn write_addr(&mut self, addr: Address, size: usize, pcrel: bool) -> std::io::Result<()>;
+pub trait InsnRead : Read{
+    fn read_addr(&mut self, size: usize) -> std::io::Result<Address>;
 }
 
-impl<I: InsnWrite> InsnWrite for &mut I {
-    fn write_addr(&mut self, addr: Address, size: usize, pcrel: bool) -> std::io::Result<()> {
-        <I as InsnWrite>::write_addr(self, addr, size, pcrel)
+impl<R: InsnRead> InsnRead for R{
+    fn read_addr(&mut self, size: usize) -> std::io::Result<Address>{
+        <R as InsnRead>::read_addr(self, size)
     }
 }
 
-pub trait InsnRead: Read {
-    fn read_addr(&mut self, size: usize, pcrel: bool) -> std::io::Result<Address>;
+pub trait InsnWrite: Write{
+    fn write_addr(&mut self, size: usize) -> std::io::Result<()>;
 }
 
-impl<I: InsnRead> InsnRead for &mut I {
-    fn read_addr(&mut self, size: usize, pcrel: bool) -> std::io::Result<Address> {
-        <I as InsnRead>::read_addr(self, size, pcrel)
+impl<W: InsnWrite> InsnWrite for W{
+    fn write_addr(&mut self, size: usize) -> std::io::Result<()>{
+        <W as InsnWrite>::write_addr(self,size)
     }
 }
