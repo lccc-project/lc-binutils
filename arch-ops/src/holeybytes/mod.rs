40d88bd61ec259a9ac41e2104644332246084ab3
ondra05 <ondra@ondra05.cz>
2023-09-27 00:50:15 +0200

    Validation and try from

--- arch-ops/src/holeybytes/mod.rs
+++ arch-ops/src/holeybytes/mod.rs
@@ -1,3 +1,7 @@
+use std::convert::TryFrom;
+
+pub mod codec;
+
 with_builtin_macros::with_builtin! {
     let $spec = include_from_root!("src/holeybytes/instructions.in") in {
         macro_rules! invoke_with_def {
@@ -6,6 +10,12 @@ with_builtin_macros::with_builtin! {
             };
         }
     }
+}
+
+macro_rules! ignore_const_one {
+    ($_:ident) => {
+        1
+    };
 }
 
 /// Create opcode enum from definition
@@ -19,6 +29,20 @@ macro_rules! opcodes {
                     #[doc = $doc]
                     [<$mnemonic:camel>] = $opcode
                 ),*
+            }
+
+            impl TryFrom<u8> for Opcode {
+                type Error = ();
+            
+                fn try_from(value: u8) -> Result<Self, Self::Error> {
+                    const INST_HIGH: u8 = 0 $(+ ignore_const_one!($mnemonic))*;
+                    if value < INST_HIGH {
+                        #[allow(unsafe_code)]
+                        Ok(unsafe { std::mem::transmute(value) })
+                    } else {
+                        Err(())
+                    }
+                }
             }
         }
     };
@@ -64,7 +88,8 @@ macro_rules! define_operands {
             pub struct $name($(pub $item),*);
         )*
 
-        pub enum Operands {
+        #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+        pub enum Operand {
             $($name($name)),*
         }
     };
@@ -92,17 +117,47 @@ define_operands! {
 invoke_with_def!(verify_ops);
 
 /// Validate if passed [`Operands`] is correct for passed [`Opcode`]
-pub fn validate_ops_for_opcode(opcode: Opcode, operands: &Operands) -> bool {
+pub const fn validate_ops_for_opcode(opcode: Opcode, operand: &Operand) -> bool {
     macro_rules! generate {
         ($($_o:expr, $mnemonic:ident, $ty:ident, $_d:literal;)*) => {
             paste::paste! {
                 match opcode {
                     $(Opcode::[<$mnemonic:camel>] =>
-                        matches!(operands, Operands::[<Ops $ty>] { .. })),*
+                        matches!(operand, Operand::[<Ops $ty>] { .. })),*
                 }
             }
         };
     }
 
     invoke_with_def! { generate }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Instruction {
+    opcode: Opcode,
+    operand: Operand,
+}
+
+impl Instruction {
+    pub const fn new(opcode: Opcode, operand: Operand) -> Option<Self> {
+        if validate_ops_for_opcode(opcode, &operand) {
+            Some(Self::new_unchecked(opcode, operand))
+        } else {
+            None
+        }
+    }
+
+    pub const fn new_unchecked(opcode: Opcode, operand: Operand) -> Self {
+        Self { opcode, operand }
+    }
+
+    #[inline]
+    pub fn opcode(&self) -> Opcode {
+        self.opcode
+    }
+
+    #[inline]
+    pub fn operand(&self) -> Operand {
+        self.operand
+    }
 }
