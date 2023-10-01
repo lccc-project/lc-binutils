99eb4cc1822c9b4b7003e8545d501c2163ad788c
ondra05 <ondra@ondra05.cz>
2023-10-01 04:21:07 +0200

    fix(binfmt): Holey Bytes compilation
    feat(binfmt): Finish implementing `from_reloc_code` for Holey Bytes

--- binfmt/src/elf64/holeybytes.rs
+++ binfmt/src/elf64/holeybytes.rs
@@ -1,4 +1,8 @@
-use super::Elf64Format;
+use {
+    super::Elf64Format,
+    crate::howto::{HowTo, HowToError},
+    arch_ops::traits::RelocCode,
+};
 
 macro_rules! gen {
     (count: $count:expr; $($variant:ident),* $(,)?) => {
@@ -14,14 +18,14 @@ macro_rules! gen {
 }
 
 gen! {
-    count: 13;
+    count: 15;
     None,
     Abs64,
     Rel16,
     Rel32,
     RelaxRel,
     GotPcrel,
-    Plt,
+    PltPcrel,
     GotPcrelRelax,
     PltRelax,
     Dynent,
@@ -48,16 +52,11 @@ impl HowTo for Elf64HoleyBytesHowTo {
         Some(match code {
             RelocCode::None => &TABLE[Self::None as usize],
             RelocCode::Abs { addr_width: 64 } => &TABLE[Self::Abs64 as usize],
-            RelocCode::BaseRel { addr_width } => todo!(),
             RelocCode::Rel { addr_width: 16 } => &TABLE[Self::Rel16 as usize],
             RelocCode::Rel { addr_width: 32 } => &TABLE[Self::Rel32 as usize],
-            RelocCode::AbsShifted { addr_width, shift } => todo!(),
-            RelocCode::RelShifted { addr_width, shift } => todo!(),
-            RelocCode::Got { addr_width } => todo!(),
-            RelocCode::RelGot { addr_wdith } => todo!(),
-            RelocCode::Plt { addr_width } => todo!(),
-            RelocCode::RelPlt { addr_width } => todo!(),
-            RelocCode::DynSymEntry { width } => todo!(),
+            RelocCode::RelGot { addr_wdith: 32 } => &TABLE[Self::GotPcrel as usize],
+            RelocCode::RelPlt { addr_width: 32 } => &TABLE[Self::PltPcrel as usize],
+            RelocCode::DynSymEntry { width: 8 } => &TABLE[Self::Dynent as usize],
             _ => return None,
         })
     }
@@ -75,7 +74,7 @@ impl HowTo for Elf64HoleyBytesHowTo {
             Rel32 => "R_HOLEYBYTES_REL32",
             RelaxRel => "R_HOLEYBYTES_RELAXREL",
             GotPcrel => "R_HOLEYBYTES_GOTPCREL",
-            Plt => "R_HOLEYBYTES_PLT",
+            PltPcrel => "R_HOLEYBYTES_PLT",
             GotPcrelRelax => "R_HOLEYBYTES_GOTPCRELRELAX",
             PltRelax => "R_HOLEYBYTES_PLTRELAX",
             Dynent => "R_HOLEYBYTES_DYNENT",
@@ -92,7 +91,7 @@ impl HowTo for Elf64HoleyBytesHowTo {
         match self {
             None => 0,
             Rel16 => 2,
-            Rel32 | GotPcrel | Plt | GotDynTpOff => 4,
+            Rel32 | GotPcrel | PltPcrel | GotDynTpOff => 4,
             RelaxRel | GotPcrelRelax | PltRelax => 5,
             Abs64 | Dynent | JumpSlot | GlobData | TpOff | DynTpOff => 8,
         }
@@ -102,7 +101,7 @@ impl HowTo for Elf64HoleyBytesHowTo {
         use Elf64HoleyBytesHowTo::*;
         match self {
             None | Abs64 | Dynent | JumpSlot | GlobData | TpOff | DynTpOff => false,
-            Rel16 | Rel32 | RelaxRel | GotPcrel | Plt | GotPcrelRelax | PltRelax | GotDynTpOff => {
+            Rel16 | Rel32 | RelaxRel | GotPcrel | PltPcrel | GotPcrelRelax | PltRelax | GotDynTpOff => {
                 true
             }
         }
@@ -113,11 +112,11 @@ impl HowTo for Elf64HoleyBytesHowTo {
         matches!(self, RelaxRel | GotPcrelRelax | PltRelax)
     }
 
-    fn relax_size(&self, addr: u128, at_addr: u128) -> Option<usize> {
+    fn relax_size(&self, _addr: u128, _at_addr: u128) -> Option<usize> {
         unimplemented!("Will be removed soon™?")
     }
 
-    fn apply(&self, addr: u128, at_addr: u128, region: &mut [u8]) -> Result<bool, HowToError> {
+    fn apply(&self, _addr: u128, _at_addr: u128, _region: &mut [u8]) -> Result<bool, HowToError> {
         todo!()
     }
 }
